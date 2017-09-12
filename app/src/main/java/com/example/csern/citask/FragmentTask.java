937243d2382281cb9b3ce6cad397c9f27f44d979
CARLOS-PC\csern <cserna1979elche@gmail.com>
2017-09-12 13:39:11 +0200

    fragment Funcionando

--- app/src/main/java/com/example/csern/citask/FragmentTask.java
+++ app/src/main/java/com/example/csern/citask/FragmentTask.java
@@ -1,24 +1,74 @@
 package com.example.csern.citask;
 
+import android.content.Context;
 import android.app.Fragment;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 /**
- * Created by csern on 03/09/2017.
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link FragmentTask.OnFragmentInteractionListener} interface
+ * to handle interaction events.
  */
-
 public class FragmentTask extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragmentmain, container);
+    private OnFragmentInteractionListener mListener;
+
+    public FragmentTask() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_fragment_task, container, false);
         return view;
-        minuto 20.15
-        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/example/csern/citask/Main2Activity.java
+++ app/src/main/java/com/example/csern/citask/Main2Activity.java
@@ -1,9 +1,10 @@
 package com.example.csern.citask;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.app.FragmentTransaction;
+import android.app.FragmentManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,10 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, FragmentTask.OnFragmentInteractionListener {
 
     private User user;
 
@@ -36,6 +36,18 @@ public class Main2Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        FragmentTask fragmentTask = new FragmentTask();
+        transaction.add(R.id.layoutmain, fragmentTask);
+        transaction.commit();
+
+        /*getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.layoutcalendar, fragmentTask)
+                .commit();*/
+
     }
 
     @Override
@@ -93,5 +105,10 @@ public class Main2Activity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
